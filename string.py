2ddfe0fcbb6c6a7eca8155d75532af3834bd04d6
Mansur550 <mansurislam59@gmail.com>
2026-04-21 20:26:48 +0600

    string

---
+++ string.py
@@ -0,0 +1,14 @@
+# String is a sequence of characters enclosed in single quotes (' ') or double quotes (" ").
+
+b="""
+Python is Dynamically Typed Language.
+It means that we do not need to declare the type of variable while creating it.
+Python interpreter automatically detects the type of variable based on the value assigned to it.
+"""
+
+a= 'This is a string'
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
