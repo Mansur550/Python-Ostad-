31fb343e2353614fea5cacbe4c7485ccd349628b
Mansur550 <mansurislam59@gmail.com>
2026-04-23 01:48:44 +0600

    String Format

---
+++ string_format.py
@@ -0,0 +1,23 @@
+
+
+# user_input =input("What is your name?")
+# a = "Good moring,{}. How are you?".format(user_input)
+# print(a)
+# print(user_input)
+
+
+# f_name = input("What is your first name?")
+# l_name =  input("What is your last name?")
+# age=input("What is your age?")
+# txt = "My name is {} {}. I am {} years old.".format(f_name, l_name, age)
+# print(txt)
+
+
+
+f_name = input("What is your first name?")
+l_name =  input("What is your last name?")
+age=input("What is your age?")
+txt = "My name is {f_name} {l_name}. I am {age} years old.".format( l_name=l_name, age=age,f_name=f_name,)
+
+txt2 = f"My name is {f_name} {l_name}. I am {age} years old."
+print(txt2)
