2148c348ba38ebb0668359d83f0fc2a409355fff
Mansur550 <mansurislam59@gmail.com>
2026-04-23 01:29:39 +0600

    string methods

---
+++ string_methos.py
@@ -0,0 +1,14 @@
+a="raHim"
+# a=a.title()
+print(a.title())
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.swapcase())
+
+# replace
+txt ="I like Bananas"
+x = txt.replace("Bananas", "apple")
+print(x)
+print(txt)
+print(txt.count("a"))
