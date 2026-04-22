eebbc5977b14b8ef8d663cad2c9a805f677a3f4a
Mansur550 <mansurislam59@gmail.com>
2026-04-22 23:46:44 +0600

    Sting=Immutable DataType

--- string_index.py
+++ string_index.py
@@ -14,3 +14,7 @@ b="Rahim"
         #-5-4-3-2-1
 print(b[-4])
 
+#String is immautable, which means you cannot change the characters in a string after it has been created. You can only create a new string with the desired changes.
+# For example, if you want to change the first character of the string "Rahim" to "M", you cannot do it directly. Instead, you can create a new string by concatenating the desired character with the rest of the original string.
+b[1]="X"
+print(b) # This will raise an error because strings are immutable.
