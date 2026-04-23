33cb8313382672b83e5da2a02d5b4054a3c28e06
Mansur550 <mansurislam59@gmail.com>
2026-04-24 01:35:37 +0600

    pos, neg or 0

--- conditional.py
+++ conditional.py
@@ -9,3 +9,16 @@ print(10==10)
 print(10==19)
 print(10!=10)
 print(10==10)
+
+
+#IS the number is positive, negative or zero?
+
+number =int(input("Enter a number: "))
+# number = 1
+
+if number > 0:
+    print ("The number is a positive number")
+elif number < 0:
+    print ("The number is a negative number ")
+else: 
+    print ("The number is zero")
