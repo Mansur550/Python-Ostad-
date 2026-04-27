5557d994498f46f8f274129267a32fe7865608ed
Mansur550 <mansurislam59@gmail.com>
2026-04-27 17:28:16 +0600

    List and Tuple

---
+++ list_tuples.py
@@ -0,0 +1,21 @@
+a=[1,2,3,4, "Rahim", "Python", 3.14, True]
+
+# a[7]= "Mansur"
+# print(a)
+# print(a[-2])
+# print(len(a))
+
+m = "Mansur"
+print(list(m))
+
+a.append(["Dristy",12,56,89])
+print(a)
+a.reverse()
+print(a)
+
+
+t=(1,2,3,4, "Rahim", "Python", 3.14, True)
+tr=tuple(reversed(t))
+print(tr)
+# t[0]=10
+print(t)
