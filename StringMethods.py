2a6368d6b890d2c83b7e8548b5507980b723b3e0
Mansur550 <mansurislam59@gmail.com>
2026-06-15 00:38:09 +0600

    Chaining methods

--- StringMethods.py
+++ StringMethods.py
@@ -16,3 +16,13 @@ print(text2.endswith("World"))    # Output: True
 print(text2.split())  # Output: ['Hello', 'World']
 
 
+sentence ="python programming is fun"
+print(sentence.count("o")) # Output: 2 (counts occurrences of 'o')
+print(sentence.find("gram"))  # Output: 10 (finds position of substring)
+print(sentence.split()) 
+
+
+# Chaining mulltiple methods
+text3 = " Java progRaMming  "
+result =text3.strip().replace("java", "Python").upper().title()
+print(result)
