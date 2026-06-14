2a7336a39970dde9fb8781ddc018f536ae6232ee
Mansur550 <mansurislam59@gmail.com>
2026-06-15 01:23:47 +0600

    Nested if else and Logical Operator

--- ConditionalStatement.py
+++ ConditionalStatement.py
@@ -28,3 +28,28 @@ if age >= 18 and has_license:
     print("You need a license to drive cars")
 else:
     print("You are too young or you don't have a license")
+
+
+# Example-2
+temperature = 8
+is_sunny = True
+
+if temperature > 15:
+    print("The weather is warm enough. Let's go out.")
+else:
+    if is_sunny:
+        print("It's cold, but sunny. Let's go out.")
+    else:
+        print("It's cold and not sunny, so it's risky to go outside.")
+
+
+# This nested condition clearly explains why going outside is risky.
+# The above code can be converted using or operator.
+
+temp= 7
+is_sunny = True
+
+if  temp >15 or is_sunny:
+    print("Let's go out.")
+else:
+    print("It's cold and not sunny, so it's risky to go outside.")
