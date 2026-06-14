7def10c46e2bd9fe4ccb5862f01605c38a1b1c68
Mansur550 <mansurislam59@gmail.com>
2026-06-15 00:20:14 +0600

    Sting functions

---
+++ StringMethods.py
@@ -0,0 +1,11 @@
+text = " hello world "
+print(text.upper())  # Output: "HELLO WORLD"
+print(text.lower()) # Output: "hello world"
+print(text.title())  
+print(text.strip())  # Output: "hello world"
+print(text.replace(" ", "_"))  # Output: "_hello_world_"
+
+
+
+text2 = "Hello World"
+print(text2.replace("World", "Bangladesh"))
