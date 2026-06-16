bb3f6a28bd8c635998902be0d7ba36cf18b7786d
Mansur550 <mansurislam59@gmail.com>
2026-06-17 01:49:16 +0600

    Palindrome Check

--- ProblemSolving_(Strings+Loop+ifelse).py
+++ ProblemSolving_(Strings+Loop+ifelse).py
@@ -8,3 +8,29 @@ nid = "1234567890"
 
 masked = nid[:2] + "*" * 6 + nid[8:]
 print(masked)
+
+
+"""
+2. 📞 Extract Country Code from Phone Number
+"""
+phone = "+880 1234 567890"
+country_code= phone.split()[0] if " " in phone else phone[:4]
+print(country_code)  # Output: "+880"
+
+"""
+3. 🔁 Palindrome Check
+
+A string that reads the same forward and backward.
+
+- racecar
+- dad
+- level
+"""
+text = "madam"
+
+reversed_text = text[::-1]
+
+if text == reversed_text:
+    print(f"{text} is a palindrome.") 
+else:
+    print(f"{text} is not a palindrome.")
