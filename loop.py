5b824d5ab403e22bc9a4f9a34079e7dbcec1803b
Mansur550 <mansurislam59@gmail.com>
2026-06-20 00:02:26 +0600

    for loop len()

---
+++ loop.py
@@ -0,0 +1,15 @@
+# Loop with sting
+word = "Python"
+
+for char in word:
+    print(char)
+
+
+# find the length of the string using for loop
+ 
+count= 0
+for char in word:
+    count += 1
+print("Length of the string:", count)
+
+
