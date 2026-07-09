bbe9b3234074ec72d64d0615a122c97e55030716
Mansur550 <mansurislam59@gmail.com>
2026-07-10 01:15:32 +0600

    tuple *

--- tuple.py
+++ tuple.py
@@ -17,12 +17,18 @@ print(x.index(5)) #gives the position of 5
 
 # tuple unpacking
 
-y= (1,3,4,"hello", "hi")
-a, b, c, d,e =y
+# y= (1,3,4,"hello", "hi")
+# a, b, c, d,e =y
 
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+Y= (1,3,"hello", "hi")
+A, B, *C =Y
+print(A)
+print(B)
+print(C)
 
