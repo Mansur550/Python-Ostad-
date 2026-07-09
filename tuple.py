4c0cb1ba4182318e4c1b95f517217fbb68578a65
Mansur550 <mansurislam59@gmail.com>
2026-07-10 01:06:05 +0600

    tuple count + index search

--- tuple.py
+++ tuple.py
@@ -2,3 +2,15 @@ t =(1,2,3,4,45)
 
 print(t)
 print(type(t))
+
+T= tuple(range(1,20))
+print(T)
+l= [10,20,30]
+print(type(l))
+L= tuple(l)
+print(type(t))
+
+#tuple
+x=(1,2,3,4,5,6)
+print(x.count(4))  #Number of 4
+print(x.index(5)) #gives the position of 5
