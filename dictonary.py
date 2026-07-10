2cef997486395a2157c2b5e34629ed272565246d
Mansur550 <mansurislam59@gmail.com>
2026-07-10 14:40:22 +0600

    Dictonary Constrcutor function

--- dictonary.py
+++ dictonary.py
@@ -1,3 +1,8 @@
 
-d={"name" : "Saiham", "age": 25, "city": "Dhaka"}
+d={"name" : "Saiham", "age": 25, "city": "Dhaka"} #{key: value}
 print(d)
+
+#Dictonary Constrcutor function
+x = dict(name="Mansur", age =25, city= "Gazipur")
+print(x) 
+print(d["age"])
