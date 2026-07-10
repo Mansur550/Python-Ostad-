e7d6227fc18e47b00136f3f7127ba6ad0cc36041
Mansur550 <mansurislam59@gmail.com>
2026-07-10 16:05:32 +0600

    Print Set with for (unorderd list)

--- IteratingOverList.py
+++ IteratingOverList.py
@@ -2,3 +2,16 @@ fruits = ["Apple", "Banana", "Mango"]
 
 for item in fruits:
     print (f"Processing {item}")
+
+
+
+fruit = ["Apple", "Banana", "Mango"]
+
+for item in fruit:
+    print (f"Processing {item}")
+
+
+fruitz = {"Apple", "Banana", "Mango"}
+
+for item in fruitz:
+    print (f"Processing {item}")
