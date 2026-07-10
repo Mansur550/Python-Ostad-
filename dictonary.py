ea329c68f3e1a4a8d319c9842e0bdc647fa6fb54
Mansur550 <mansurislam59@gmail.com>
2026-07-10 15:10:36 +0600

    Dictonary methods

--- dictonary.py
+++ dictonary.py
@@ -6,3 +6,21 @@ print(d)
 x = dict(name="Mansur", age =25, city= "Gazipur")
 print(x) 
 print(d["age"])
+# Dictonary  is mutable
+d["work"]= "Ostad"
+d["age"]= 35
+print(d)
+
+d = {'name': 'Saihan', 'age': 20, 'work': 'Ostad', 'city': 'dhaka', 'phone': 12345}
+
+
+print(d.get("country", "BD"))
+
+
+# A dictionary works like a search engine.
+# You give a key, and it instantly returns the value.
+
+student_info = {
+    "Shovon": {"roll": 123, "city": "Dhaka", "age": 30},
+    "Sadia": {"roll": 127, "city": "New York", "age": 29}
+}
