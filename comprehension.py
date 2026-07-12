37750e4f87b012a8596f96590f9c114ce06e8663
Mansur550 <mansurislam59@gmail.com>
2026-07-12 20:18:19 +0600

    Comprehension on dict

--- comprehension.py
+++ comprehension.py
@@ -7,3 +7,19 @@ for num in numbers:
     squared_numbers.append(num**2)
 #print squared number
 print(squared_numbers)
+# Comprehensions
+sq_num= [num**2 for num in numbers]
+print(sq_num) 
+
+
+
+#Comprehension for dictonary
+price ={"Rice": 60, "Suger": 100, "Salt": 80}
+
+# discount_price= {}
+# for item , price in price.items():
+#    discount_price.update({item: price*0.9})
+# print(f"After discount: {discount_price}")
+
+discount_price= {item:price*0.9 for item , price in price.items()}
+print(f"After discount: {discount_price}")
