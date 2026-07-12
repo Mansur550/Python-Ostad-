be6b9e362602700dcf0da71f1faf289083bc84d8
Mansur550 <mansurislam59@gmail.com>
2026-07-12 20:26:02 +0600

    even

--- comprehension.py
+++ comprehension.py
@@ -23,3 +23,16 @@ price ={"Rice": 60, "Suger": 100, "Salt": 80}
 
 discount_price= {item:price*0.9 for item , price in price.items()}
 print(f"After discount: {discount_price}")
+
+
+#Even number
+numbers = list(range(1,21))
+
+even_numbers= []
+
+for num in numbers:
+    if num %2==0:
+        even_numbers.append(num)
+
+print(even_numbers)
+      
