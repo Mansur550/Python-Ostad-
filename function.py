c2417b631b2294b09dbd2686087012afec473f53
Mansur550 <mansurislam59@gmail.com>
2026-07-25 23:21:02 +0600

    Fuction solution

--- function.py
+++ function.py
@@ -15,3 +15,13 @@
 # - Code duplication
 # - Hard to maintain
 # - If tax rate changes, you must update everywhere
+
+# Solution->
+
+def calculate_final_price(price, item):
+    tax_rate= 0.25
+    total_price = price+ (price*tax_rate)
+    print(f"Final price of {item} is: ",total_price )
+
+calculate_final_price(1000, "Tv")
+calculate_final_price(3500, "Solar Panel")
